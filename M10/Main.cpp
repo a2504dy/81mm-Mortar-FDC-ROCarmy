ed0b6c77d41a21d96e6fc437735cf77c0ff933ac
智鈞 楊 <pginoron@msn.com>
2021-03-24 22:19:18 +0800

    新增效力射功能，因無數據無法確定演算法是否正確

--- M10/Main.cpp
+++ M10/Main.cpp
@@ -22,10 +22,10 @@ private:
 	double distant, direct, dir;
 	double x, y;
 	// Next Pointer
-	CR *next;
+	CR* next;
 
 public:
-	
+
 	CR() :distant(0), direct(0), dir(0), x(0), y(0), next(NULL) {};
 	CR(double a, double b) : distant(a), direct(b), dir(0), x(0), y(0), next(NULL) {};
 	CR(double a, double b, double c) : distant(a), direct(b), dir(c), x(0), y(0), next(NULL) {};
@@ -37,21 +37,21 @@ class LinkedList
 {
 private:
 	int size;
-	CR *Head;            // list的第一個node
-	CR *Current;
+	CR* Head;            // list的第一個node
+	CR* Current;
 public:
 	LinkedList() :size(0), Head(NULL), Current(Head) {};
-	
+
 	void NewCR(double a1, double b1, double c1, double d1, double e1);			// 在list的尾巴新增node
 	void ModiCR(double a1, double b1, double c1, double d1, double e1);
-	void GetCoord(Obser_value *p);			// 提取座標
+	void GetCoord(Obser_value* p);			// 提取座標
 	void PrintCR();				// Print this node
 	void PrintList();           // 印出list的所有資料
 	void Clear();               // 把整串list刪除
 
 };
 
-void LinkedList::NewCR(double a1, double b1, double c1, double d1, double e1) 
+void LinkedList::NewCR(double a1, double b1, double c1, double d1, double e1)
 {
 	if (Head == NULL)
 	{
@@ -65,7 +65,7 @@ void LinkedList::NewCR(double a1, double b1, double c1, double d1, double e1)
 		if (Head->next == NULL)
 			Head->next = Current;
 	}
-	
+
 	size++;
 }
 
@@ -89,7 +89,7 @@ void LinkedList::ModiCR(double a1, double b1, double c1, double d1, double e1)
 	Current->y = e1;
 }
 
-void LinkedList::GetCoord(Obser_value *p)
+void LinkedList::GetCoord(Obser_value* p)
 {
 	p->Ob_dir = Current->x;
 	p->Ob_dist = Current->y;
@@ -97,13 +97,13 @@ void LinkedList::GetCoord(Obser_value *p)
 
 void LinkedList::PrintList() {
 
-	CR *previous = Head;
+	CR* previous = Head;
 	if (Head == NULL)
 		cout << "內容空白" << endl << endl;
 
 	int temp = 1;
-	while (previous) 
-	{	
+	while (previous)
+	{
 		if (temp == 1)
 			cout << endl << "最初射擊口令:" << endl;
 		else
@@ -144,7 +144,7 @@ int main()
 	double ot_a, ot_d, om_a, om_d, mt_a, mt_d;
 	double o_x, o_y, t_x, t_y, m_x, m_y;
 	o_x = 0; o_y = 0; t_x = 0; t_y = 0; m_x = 0; m_y = 0;
-	
+
 	cout << "圖解最初諸元" << endl << endl;
 	cout << "請輸入 OT方位角" << endl;
 	cout << "若要直接輸入MT方位角與距離請輸入 -1 :";
@@ -157,7 +157,7 @@ int main()
 		cin >> om_a;
 		cout << "請輸入OM距離 :";
 		cin >> om_d;
-		
+
 		// Calculate MT
 		o_x = -om_d * cos((90.0 - om_a * Ratio_mil) * Radi);
 		o_y = -om_d * sin((90.0 - om_a * Ratio_mil) * Radi);
@@ -179,12 +179,12 @@ int main()
 		t_x = mt_d * cos((90.0 - mt_a * Ratio_mil) * Radi);
 		t_y = mt_d * sin((90.0 - mt_a * Ratio_mil) * Radi);
 	}
-	
+
 	List.NewCR(mt_d, mt_a, 0, t_x, t_y);
 	List.PrintCR();
-// part 2 Fire on Effect
+	// part 2 Fire on Effect
 	cout << "檢驗點試射或效力射" << endl;
-		
+
 	bool checkpoint = 1;
 	while (checkpoint)
 	{
@@ -194,9 +194,9 @@ int main()
 		cout << "觀測結果 : 1" << endl;
 		cout << "修正報告 : 2" << endl;
 		cin >> sel;
-		
+
 		// Calculate Target
-		if (sel != 0)
+		if (sel == 1 || sel == 2)
 		{
 			if (ot_a == -1)
 			{
@@ -204,7 +204,7 @@ int main()
 				cin >> ot_a;
 				cin >> ot_d;
 			}
-			
+
 			switch (sel)
 			{
 			case 1:
@@ -221,32 +221,76 @@ int main()
 				break;
 			}
 			cout << "修正報告: 向右(+)左(-) " << o.Ob_dir << " 加多(+)減少(-) " << o.Ob_dist << endl;
-		// Calculate x,y
+			// Calculate x,y
+
+				//需模組化
 			double x, y;//C -s sC
 			List.GetCoord(&Coord);
 			x = Coord.Ob_dir + o.Ob_dir * cos((ot_a * Ratio_mil) * Radi) + o.Ob_dist * sin((ot_a * Ratio_mil) * Radi);
 			y = Coord.Ob_dist - o.Ob_dir * sin((ot_a * Ratio_mil) * Radi) + o.Ob_dist * cos((ot_a * Ratio_mil) * Radi);
-			
+
 			double angle, distant;
 			angle = (atan2(x, y) / Radi) / Ratio_mil;
-				
+
 			if (angle < 0)
 				angle = 6400 + angle;
-		
+
 			distant = pow(pow(x, 2) + pow(y, 2), 0.5);
 
 			List.NewCR(distant, angle, angle - mt_a, x, y);
 			List.PrintCR();
+			//
 		}
-		
-		
+		if (sel == 0)// 效力射
+		{
+			double x, y;
+			bool fire;
+			cout << "選擇射擊法" << endl;
+			cout << "固定射 :0" << endl;
+			cout << "集火射 :1" << endl;
+			cin >> fire;
+
+
+			cout << "是否修改目標 (0/1) :";
+			cin >> fire;
+			if (fire)
+			{
+				cout << "請輸入新OT方位角 :";
+				cin >> ot_a;
+				cout << "請輸入新目標與檢驗點之關係";
+				cout << "向右 (+) 向左 (-) :";
+				cin >> o.Ob_dir;
+				cout << "加多 (+) 減少 (-) :";
+				cin >> o.Ob_dist;
+			}
+			else
+			{
+				o.Ob_dir = 0; o.Ob_dist = 0;
+			}
+			List.GetCoord(&Coord);
+			x = Coord.Ob_dir + o.Ob_dir * cos((ot_a * Ratio_mil) * Radi) + o.Ob_dist * sin((ot_a * Ratio_mil) * Radi);
+			y = Coord.Ob_dist - o.Ob_dir * sin((ot_a * Ratio_mil) * Radi) + o.Ob_dist * cos((ot_a * Ratio_mil) * Radi);
+
+			double angle, distant;
+			angle = (atan2(x, y) / Radi) / Ratio_mil;
+
+			if (angle < 0)
+				angle = 6400 + angle;
+
+			distant = pow(pow(x, 2) + pow(y, 2), 0.5);
+
+			List.NewCR(distant, angle, angle - mt_a, x, y);
+			List.PrintCR();
+
+		}
+
 		cout << "繼續(0/1)" << endl;
 		cin >> checkpoint;
 	}
 	List.PrintList();
 }
 
-void OB(Obser_value* p,double OT_dist)
+void OB(Obser_value* p, double OT_dist)
 {
 	double dist, dir;
 	static int time = 1;
@@ -257,6 +301,6 @@ void OB(Obser_value* p,double OT_dist)
 	cin >> dir;
 	p->Ob_dir = -OT_dist / 1000 * dir;
 	// bracketing method
-	p->Ob_dist = 100 * pow(2,(ceil(OT_dist / 1000)-1)) * (-dist / abs(dist)) / time;
+	p->Ob_dist = 100 * pow(2, (ceil(OT_dist / 1000) - 1)) * (-dist / abs(dist)) / time;
 	time++;
 }
